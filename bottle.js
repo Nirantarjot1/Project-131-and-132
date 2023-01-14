5ade6b9a48c8014291e6cb9a33e4759f705b4e9a
Nirantarjot1 <95531047+Nirantarjot1@users.noreply.github.com>
2023-01-14 11:07:37 +0530

    Add files via upload

---
+++ bottle.js
@@ -0,0 +1,37 @@
+img="";
+stats="";
+
+function setup(){
+    canvas=createCanvas(640 , 420);
+    canvas.center();
+}
+
+function preload(){
+    img=loadImage("bottle.jpg")
+}
+
+function draw(){
+    image(img , 0 , 0 , 640 , 420);
+    fill("#fc4235");
+    text("bottle" , 45 , 75);
+    noFill();
+    stroke("#fc4235");
+    rect(30 , 60 , 550 , 350); 
+}
+
+objectDetector=ml5.objectDetector('cocossd' , modelLoaded);
+document.getElementById("stats").innerHTML="Status  : Detecting Objects ";
+
+
+function modelLoaded(){
+    console.log("Model is Loaded");
+    stats=true;
+    objectDetector.detect(img , gotResult);
+}
+
+function gotResult(error ,  results){
+    if(error){
+        console.log(error);
+    }
+console.log(results);
+}
